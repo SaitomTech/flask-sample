343ae326bca721879932f87a5f6cf32f414d2744
SaitomTech <57802435+SaitomTech@users.noreply.github.com>
2022-05-03 20:15:42 +0900

    add: contact route

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -30,3 +30,20 @@ with app.test_request_context():
 
     # /name/taro?page=taro
     print(url_for("show_name", name="taro", page="1"))
+
+
+@app.route("/contact")
+def contact():
+    """contact."""
+    return render_template("contact.html")
+
+
+@app.route("/contact/complete", methods=["GET", "POST"])
+def contact_complete():
+    """contact completed."""
+    if request.method == "POST":
+        # メール送信
+
+        # contactエンドポイントにリダイレクト
+        return redirect(url_for("contact_complete"))
+    return render_template("contact_complete.html")
